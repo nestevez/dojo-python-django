364a244ae33ee869faf12b898f65a3d8e0820228
Natalia <nataliae.dev@gmail.com>
2017-09-05 13:49:11 -0700

    Added Max and Min function

--- learnPortal/stringsAndLists.py
+++ learnPortal/stringsAndLists.py
@@ -8,9 +8,21 @@ def find_and_replace(str, word, repl):
     print "The first time", word, "appears in the string is at index", pos
     return string.replace(str, word, repl) #creates new string, replacing the word with the substitute
 
-stringy = "It's thanksgiving day. It's my birthday,too!"
-lookfor = 'day'
-replacewith = 'month'
+# stringy = "It's thanksgiving day. It's my birthday,too!"
+# lookfor = 'day'
+# replacewith = 'month'
+#
+# new_string = find_and_replace(stringy,lookfor,replacewith)
+# print new_string
 
-new_string = find_and_replace(stringy,lookfor,replacewith)
-print new_string
+
+#Min and Max
+def min_and_max(list):
+    '''Prints the max and min values of a given list'''
+    maxi = max(list)
+    mini = min(list)
+    print "The maximum value in the list is", maxi,"and the minimum value is", mini
+    return
+
+# some = ['a',24,True,'rock',743]
+# min_and_max(some)
